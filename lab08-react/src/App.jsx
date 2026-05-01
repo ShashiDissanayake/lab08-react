f8e4ba1ca70a7dede049aae37a332dbe7ed20717
ShashiDissanayake <sewminishashikala781@gmail.com>
2026-05-01 23:08:36 +0530

    Completed Exercise 02

--- lab08-react/src/App.jsx
+++ lab08-react/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import Greeting from "./components/Greeting";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
+      <NavBar />
       <Greeting />
     </>
   )
---
+++ lab08-react/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+function NavBar(){
+    return(
+        <nav className="navbar">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
